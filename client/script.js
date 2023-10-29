c9aa4244307caa54ffbd08d23d7009497d4b50de
Jace Roell <jace.roell@hotmail.com>
2023-10-28 21:53:04 -0400

    changed ports to fit aws

--- client/script.js
+++ client/script.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000');
+//const socket = io('http://localhost:3000');
+const socket = io('https://jxdmrpment.us-east-2.awsapprunner.com:3000');
+
 
 localStorage.setItem('socketID', socket.id);
 
